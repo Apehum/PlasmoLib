a4776018adbb1e7d5489fd1cd62bc8d528c5ed47
Apehum <apehumchik@gmail.com>
2020-11-19 11:03:39 +0800

    -поддержка Sponge потому что я Pepega

--- src/main/java/su/plo/lib/PlasmoLibSponge.java
+++ src/main/java/su/plo/lib/PlasmoLibSponge.java
@@ -1,27 +1,27 @@
-package su.plo.lib;
-
-import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.game.GameReloadEvent;
-import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
-import org.spongepowered.api.plugin.Plugin;
-import su.plo.lib.mysql.MySQL;
-import su.plo.lib.nats.NatsLib;
-
-@Plugin(
-        id = "plasmolib",
-        name = "su.plo.lib.PlasmoLibSponge"
-)
-public class PlasmoLibSponge {
-
-    @Listener
-    public void onReload(GameReloadEvent e) {
-        MySQL.disconnect();
-        NatsLib.disconnect();
-    }
-
-    @Listener
-    public void onStop(GameStoppingServerEvent e) {
-        MySQL.disconnect();
-        NatsLib.disconnect();
-    }
-}
+//package su.plo.lib; TODO поддержка Sponge
+//
+//import org.spongepowered.api.event.Listener;
+//import org.spongepowered.api.event.game.GameReloadEvent;
+//import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+//import org.spongepowered.api.plugin.Plugin;
+//import su.plo.lib.mysql.MySQL;
+//import su.plo.lib.nats.NatsLib;
+//
+//@Plugin(
+//        id = "plasmolib",
+//        name = "su.plo.lib.PlasmoLibSponge"
+//)
+//public class PlasmoLibSponge {
+//
+//    @Listener
+//    public void onReload(GameReloadEvent e) {
+//        MySQL.disconnect();
+//        NatsLib.disconnect();
+//    }
+//
+//    @Listener
+//    public void onStop(GameStoppingServerEvent e) {
+//        MySQL.disconnect();
+//        NatsLib.disconnect();
+//    }
+//}
